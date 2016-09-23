ae1130dde768136c612552701573a2b6d8af4adc
jxmelody <jmelody@foxmail.com>
2016-09-23 10:25:08 +0800

    Create cccjson.h

---
+++ cccjson.h
@@ -0,0 +1,8 @@
+#ifndef cccjson__h
+#define cccjson__h
+
+//extern struct jsonvalue parseJson(char * pMsg);
+extern char * makeJson_regcode(char *r,int c);
+extern char * makeJson_logfi(char *l,int c);
+extern char * makeJson_sign(int return_sign);
+#endif
